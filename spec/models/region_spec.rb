365c49e18b1bd59e6f263a0a0d720f9b11c81921
priscillashort <pshort2@cocc.edu>
2020-04-07 09:42:38 -0700

    Add a couple initialization tests for Region

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -2,4 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Region, type: :model do
 
+    it 'exists' do
+        Region.new
+    end
+
+    it 'has a string representation that is the name' do
+        r = Region.new(name: 'FAKE')
+        expect(r.to_s).to eq('FAKE')
+    end
+
 end
