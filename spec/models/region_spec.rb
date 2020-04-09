cec16728ed5b7208dde12cad757820e156cbb4be
priscillashort <pshort2@cocc.edu>
2020-04-09 09:43:30 -0700

    Add a couple characterization tests for region and ticket

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -2,13 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Region, type: :model do
 
-    it 'exists' do
-        Region.new
-    end
+  it 'exists' do
+    Region.new
+  end
 
-    it 'has a string representation that is the name' do
-        r = Region.new(name: 'FAKE')
-        expect(r.to_s).to eq('FAKE')
-    end
+  it 'has a string representation that is the name' do
+    r = Region.new(name: 'FAKE')
+    expect(r.to_s).to eq('FAKE')
+  end
+
+  describe 'relationships' do
+    it { should have_many(:tickets) }
+  end
 
 end
--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -2,4 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
 
+  describe 'relationships' do
+    it { should belong_to(:region) }
+  end
+
+  #This is an alternate syntax for the same test
+  #it 'Should belong to a region' do
+  #    ticket = Ticket.new
+  #    expect(ticket).to respond_to(:region)
+  #end
+
 end
