abc2f65c19f799ebd45e0adfafec39634a8125f8
priscillashort <pshort2@cocc.edu>
2020-04-09 09:52:51 -0700

    Add characterization tests for organization

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
 
+  describe 'relationships' do
+    it { should have_many(:users) }
+    it { should have_many(:tickets) }
+    it { should have_and_belong_to_many(:resource_categories) }
+  end
+
 end
