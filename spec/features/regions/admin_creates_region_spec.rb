94cadcc93d2c63e8a11575828b170595ed85b528
priscillashort <pshort2@cocc.edu>
2020-04-14 09:54:31 -0700

    Add commented out feature test and model diagram

--- spec/features/regions/admin_creates_region_spec.rb
+++ spec/features/regions/admin_creates_region_spec.rb
@@ -2,4 +2,17 @@ require 'rails_helper'
 
 RSpec.describe 'Creating a Region', type: :feature do
 
+	# let (:admin){User.create(:role = 'admin')}
+
+  # context 'As an admin' do
+  #   specify 'I can create a region and see it in the list' do
+	# 		sign_in(admin)
+	# 		visit new_region_path
+	# 		fill_in 'name', with: 'Fake Region'
+	# 		click 'Save'
+	# 		visit regions_both
+	# 		expect(page).to have_content('Fake Region')
+  #   end
+  # end
+
 end
