81f128c840ad95a6c6058675ab67b4873a392507
priscillashort <pshort2@cocc.edu>
2020-04-21 10:06:20 -0700

    Use should keyword for region validation tests

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -15,16 +15,9 @@ RSpec.describe Region, type: :model do
   end
 
   describe 'validations' do
-    #it { should validate_presence_of(:name) }
-    it 'validates name' do
-      expect(region).to validate_presence_of(:name)
-    end
-    it 'validates length of name' do
-      expect(region).to validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create)
-    end
-    it 'validates uniqueness of name' do
-      expect(region).to validate_uniqueness_of(:name).case_insensitive
-    end
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create) }
+    it { should validate_uniqueness_of(:name).case_insensitive }
   end
 
   describe '#to_s' do
