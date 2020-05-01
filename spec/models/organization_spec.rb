c43756fbbb400d8cfac85d0ae4b5fc3354571ea1
Taylor Slye <slyet@oregonstate.edu>
2020-05-01 16:50:05 -0700

    add method test for orginization approval, not yet passing

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -54,5 +54,10 @@ RSpec.describe Organization, type: :model do
   end
 
   # TODO: Add class method tests
-
+  describe 'approve' do
+    let(:organization){build_stubbed(:organization, status: :rejected)}
+    it 'using approve on an org should approve it' do
+      expect(organization.status).to eq(:approved)
+    end
+  end
 end
