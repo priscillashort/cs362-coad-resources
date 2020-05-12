1595d800bdbaaa922cc743fc8a5b8fe014c09097
priscillashort <pshort2@cocc.edu>
2020-05-12 08:55:28 -0700

    Add commented fake and mock example

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -13,3 +13,36 @@ module DashboardHelper
   end
 
 end
+
+# describe '#dashbaord_for' do
+
+#   class FakeOrganization
+#     def submitted?
+#       true
+#     end
+#   end
+
+#   class FakeUser
+#     def admin?
+#       true
+#     end
+
+#     def organization
+#       FakeOrganization.new
+#     end
+#   end
+
+#   it 'returns admin_dashboard' do
+#     user = FakeUser.new
+#     user = double()
+#     user.stub(:admin?, true)
+#     expect(dashboard_for(user)).to eq('admin_dashboard')
+#   end
+
+#   it 'returns organization_submitted_dashboard' do
+#     user = FakeUser.new
+#     user.stub(:admin?, false)
+#     expect(dashboard_for(user)).to eq('organization_submitted_dashboard')
+#   end
+
+# end
